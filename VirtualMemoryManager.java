a5d80504111f4075fc4fcecb3f523e5a9199f01a
Manny Tenorio <tenoriom54@yahoo.com>
2016-05-04 01:37:30 -0400

    finish report

--- VirtualMemoryManager.java
+++ VirtualMemoryManager.java
@@ -14,12 +14,12 @@ public class VirtualMemoryManager{
     
     //generate Large process file, analysis purposes
     //"algorithm", "page size 32-512B (must be a power of two) (check that it is in that range)", "Input file name"
-    //alg.manager("opra", "512", "Small-Input-Processess.txt");
-    //alg.manager("fifo", "512", "Small-Input-Processess.txt");
-    //alg.manager("lru", "512", "Small-Input-Processess.txt");
-    //alg.manager("sca", "512", "Small-Input-Processess.txt");
-    //alg.manager("esca", "512", "Small-Input-Processess.txt");
-    //alg.manager("hybrid", "512", "Small-Input-Processess.txt");
+    //alg.manager("opra", "512", "Large-Input-Processess.txt");
+    //alg.manager("fifo", "256", "Large-Input-Processess.txt");
+    alg.manager("lru", "32", "Large-Input-Processess.txt");
+    //alg.manager("sca", "512", "Large-Input-Processess.txt");
+    //alg.manager("esca", "512", "Large-Input-Processess.txt");
+    //alg.manager("hybrid", "512", "Large-Input-Processess.txt");
     
   }//end of Main
 }//end of Virtual-Memory-Manager
